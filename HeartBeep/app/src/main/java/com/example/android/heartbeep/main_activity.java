a01bbf8b7760f0e112347c473c4e5c8c0d56d0c9
Teeyan <dantian00777@yahoo.com>
2017-02-25 22:07:48 -0600

    start of backend
    is commit working?

--- HeartBeep/app/src/main/java/com/example/android/heartbeep/main_activity.java
+++ HeartBeep/app/src/main/java/com/example/android/heartbeep/main_activity.java
@@ -4,14 +4,41 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.SharedPreferences;
 
 
 public class main_activity extends ActionBarActivity {
+
+    //User Passcode
+    static protected String passcode = "";
+
+    //User E-Mail
+    static protected String username = "";
+
+    SharedPreferences prefs = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activity);
+
+        //Check for first run -> display information
+        prefs = getSharedPreferences("com.example.android.heartbeep", MODE_PRIVATE);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        if(prefs.getBoolean("firstrun",true)){
+            //IF IT IS THE FIRST RUN....
+
+            //pop up dialog telling you information!
+
+
+
+
+            prefs.edit().putBoolean("firstrun", false);
+        }
     }
 
     @Override
